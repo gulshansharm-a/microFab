f8e626b564f1ffa72b78b28fb822b107b3d5b245
nivivirat <imniveditha1188@gmail.com>
2023-09-06 18:41:25 +0530

    mobile menu bug fixed

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -61,10 +61,8 @@ const Header = () => {
         isMenuOpen ? "bg-white" : "bg-gradient-to-r from-[#e1e1e1] to-[#8ca7ab]"
       } w-screen`}
       style={
-        ( window.innerWidth >= 768 ) ||
-        (!isMenuOpen && 
-          window.innerWidth < 768 && 
-          location.pathname !== "/")
+        window.innerWidth >= 768 ||
+        (!isMenuOpen && window.innerWidth < 768 && location.pathname !== "/")
           ? { background: "#FFFFFF" }
           : {}
       }
@@ -72,19 +70,37 @@ const Header = () => {
       <nav className="relative mx-4 md:mx-20 xl:mx-32 2xl:mx-40 flex flex-row items-center justify-between">
         <div className="font-bold md:text-[22px] text-[18px]">Logo</div>
         <div className="font-light relative">
-          <ul className="hidden md:flex md:gap-14 md:text-[18px] md:text[16px] z-20
-          ">
-            <li className={`${location.pathname === "/" ? "font-extrabold border-b-2 border-black " : ""}`}>
+          <ul
+            className="hidden md:flex md:gap-14 md:text-[18px] md:text[16px] z-20
+          "
+          >
+            <li
+              className={`${
+                location.pathname === "/"
+                  ? "font-extrabold border-b-2 border-black "
+                  : ""
+              }`}
+            >
               <Link to="/" onClick={toggleMenu}>
                 Home
               </Link>
             </li>
-            <li className={`${location.pathname === "/about" ? "font-extrabold border-b-2 border-black " : ""}`}>
+            <li
+              className={`${
+                location.pathname === "/about"
+                  ? "font-extrabold border-b-2 border-black "
+                  : ""
+              }`}
+            >
               <Link to="/about" onClick={toggleMenu}>
                 About Us
               </Link>
             </li>
-            <li className={`relative cursor-pointer ${location.pathname === "/Technology" ? "font-extrabold " : ""}`}>
+            <li
+              className={`relative cursor-pointer ${
+                location.pathname === "/Technology" ? "font-extrabold " : ""
+              }`}
+            >
               <div
                 className={`flex items-center cursor-pointer ${
                   isTechnologyDropdownOpen ? "font-bold" : ""
@@ -144,7 +160,11 @@ const Header = () => {
                 </div>
               )}
             </li>
-            <li className={`relative cursor-pointer ${location.pathname === "/Applications" ? "font-extrabold " : ""}`}>
+            <li
+              className={`relative cursor-pointer ${
+                location.pathname === "/Applications" ? "font-extrabold " : ""
+              }`}
+            >
               <div
                 className={`flex items-center cursor-pointer ${
                   isApplicationDropdownOpen ? "font-bold" : ""
@@ -214,17 +234,41 @@ const Header = () => {
               )}
             </li>
             <li>
-              <Link to="/articles" onClick={toggleMenu} className={`${location.pathname === "/articles" ? "font-extrabold border-b-2 border-black " : ""}`}>
+              <Link
+                to="/articles"
+                onClick={toggleMenu}
+                className={`${
+                  location.pathname === "/articles"
+                    ? "font-extrabold border-b-2 border-black "
+                    : ""
+                }`}
+              >
                 Articles
               </Link>
             </li>
             <li>
-              <Link to="/blogs" onClick={toggleMenu} className={`${location.pathname === "/blogs" ? "font-extrabold border-b-2 border-black " : ""}`}>
+              <Link
+                to="/blogs"
+                onClick={toggleMenu}
+                className={`${
+                  location.pathname === "/blogs"
+                    ? "font-extrabold border-b-2 border-black "
+                    : ""
+                }`}
+              >
                 Blogs
               </Link>
             </li>
             <li>
-              <Link to="/TP" onClick={toggleMenu} className={`${location.pathname === "/TP" ? "font-extrabold border-b-2 border-black " : ""}`}>
+              <Link
+                to="/TP"
+                onClick={toggleMenu}
+                className={`${
+                  location.pathname === "/TP"
+                    ? "font-extrabold border-b-2 border-black "
+                    : ""
+                }`}
+              >
                 Terms & policies
               </Link>
             </li>
@@ -234,7 +278,15 @@ const Header = () => {
           <ul className="flex flex-col justify-center items-center">
             <img src={contact} alt="Contact" className="w-4 h-4 mr-2" />
             <li>
-              <Link to="/contact" onClick={toggleMenu} className={`${location.pathname === "/contact" ? "font-extrabold border-b-2 border-black " : ""}`}>
+              <Link
+                to="/contact"
+                onClick={toggleMenu}
+                className={`${
+                  location.pathname === "/contact"
+                    ? "font-extrabold border-b-2 border-black "
+                    : ""
+                }`}
+              >
                 Contact
               </Link>
             </li>
@@ -294,40 +346,42 @@ const Header = () => {
 
               {/* tech in mobile */}
               {isTechnologyDropdownOpen && (
-                <div className="md:absolute mt-2 bg-white border border-gray-300 p-4 rounded-lg shadow-md h-[250px] w-[350px] md:w-[739px] mx-auto md:left-1/2 md:transform md:-translate-x-1/2">
-                  <div className="flex flex-row gap-[8px] md:gap-[16px]">
+                <Link to="/Technology" onClick={toggleMenu}>
+                  <div className="md:absolute mt-2 bg-white border border-gray-300 p-4 rounded-lg shadow-md h-[250px] w-[350px] md:w-[739px] mx-auto md:left-1/2 md:transform md:-translate-x-1/2">
+                    <div className="flex flex-row gap-[8px] md:gap-[16px]">
+                      <Card
+                        content="Blow-Fill-Seal"
+                        img={img1}
+                        isSelected={selectedTechnologyCard === "Blow-Fill-Seal"}
+                        onClick={() =>
+                          handleTechnologyCardClick("Blow-Fill-Seal")
+                        }
+                      />
+                      <Card
+                        content="Form-Fill-Seal"
+                        img={img2}
+                        isSelected={selectedTechnologyCard === "Form-Fill-Seal"}
+                        onClick={() =>
+                          handleTechnologyCardClick("Form-Fill-Seal")
+                        }
+                      />
+                    </div>
+
                     <Card
-                      content="Blow-Fill-Seal"
-                      img={img1}
-                      isSelected={selectedTechnologyCard === "Blow-Fill-Seal"}
-                      onClick={() =>
-                        handleTechnologyCardClick("Blow-Fill-Seal")
+                      content="Injection-Stretch-Blow-Molding"
+                      img={img3}
+                      isSelected={
+                        selectedTechnologyCard ===
+                        "Injection-Stretch-Blow-Molding"
                       }
-                    />
-                    <Card
-                      content="Form-Fill-Seal"
-                      img={img2}
-                      isSelected={selectedTechnologyCard === "Form-Fill-Seal"}
                       onClick={() =>
-                        handleTechnologyCardClick("Form-Fill-Seal")
+                        handleTechnologyCardClick(
+                          "Injection-Stretch-Blow-Molding"
+                        )
                       }
                     />
                   </div>
-
-                  <Card
-                    content="Injection-Stretch-Blow-Molding"
-                    img={img3}
-                    isSelected={
-                      selectedTechnologyCard ===
-                      "Injection-Stretch-Blow-Molding"
-                    }
-                    onClick={() =>
-                      handleTechnologyCardClick(
-                        "Injection-Stretch-Blow-Molding"
-                      )
-                    }
-                  />
-                </div>
+                </Link>
               )}
             </li>
             <li className="relative cursor-pointer">
