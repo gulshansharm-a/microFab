b0dc530797f467335f0b4f91907635735bccc2c5
nivivirat <imniveditha1188@gmail.com>
2023-09-13 21:50:19 +0530

    options part animation process started

--- src/components/Home/Options/OptionButtons.js
+++ src/components/Home/Options/OptionButtons.js
@@ -5,9 +5,15 @@ import arrow from "../../../assets/Icons/arrow.svg";
 import img1 from "../../../assets/Icons/air.svg";
 import seal from "../../../assets/Options/seal.svg";
 import fill from "../../../assets/Options/fill.svg";
+import injection from "../../../assets/Options/injection.svg";
+import molding from "../../../assets/Options/molding.svg";
+import stretch from "../../../assets/Options/stretch.svg";
+import form from "../../../assets/Options/form.svg";
+import blow_small from "../../../assets/Options/blow_small.svg";
 
 // the data are fetched from a json file
 import contents from "./content.json"; // Adjust the path accordingly
+import Moulds from "../right/Moulds";
 
 export default function OptionsButtons() {
   const options = ["B-F-S", "F-F-S", "I-S-B-M"];
@@ -42,7 +48,7 @@ export default function OptionsButtons() {
           <div className="md:text-[40px] text-[30px] font-bold">
             {selectedContent?.topic}
           </div>
-          <div className="text-center custom-font md:text-[12px]">
+          <div className="text-center custom-font md:text-[14px]">
             {selectedContent?.content}
           </div>
         </div>
@@ -57,50 +63,116 @@ export default function OptionsButtons() {
 
         <div className="flex flex-col md:flex-row md:justify-between md:place-items-center text-left">
           <div className=" flex flex-col md:flex-col md:w-[600px] md:h-[300px]">
-
-
-            {/* flow chart for mobile*/}
+            {/* Flow chart for mobile */}
             <div className="md:hidden justify-center flex md:flex-row md:mt-6 place-items-center md:gap-1 gap-1 mt-6 mb-6">
-              <div className="md:p-2 rounded-full bg-[#8AA6AA] h-10 w-10 justify-center flex place-items-center">
-                <img className="" src={img1}></img>
+              {selectedContent?.sequence.map((step, index) => (
+                <React.Fragment key={index}>
+                  <div
+                    className={`md:p-2 rounded-full ${
+                      index === 0
+                        ? "bg-[#8AA6AA] border border-[#8AA6AA]"
+                        : "bg-white border border-[#8AA6AA]"
+                    } 
+                    ${currentOption === "I-S-B-M" ? "h-5 w-5" : "h-10 w-10"}
+                    justify-center flex place-items-center`}
+                  >
+                    <img
+                      src={
+                        step === "blow"
+                          ? img1
+                          : step === "fill"
+                          ? fill
+                          : step === "seal"
+                          ? seal
+                          : step === "Injection"
+                          ? injection
+                          : step === "Stretch"
+                          ? stretch
+                          : step === "Blow"
+                          ? blow_small
+                          : step === "Form"
+                          ? form
+                          : step === "Molding"
+                          ? molding
+                          : img1
+                      }
+                      alt={step}
+                    ></img>
+                  </div>
+                  <p
+                    className={`font-bold text-[#8AA6AA] ${
+                      index === 0
+                        ? "font-['ClashDisplayBold'] text-[#8AA6AA]"
+                        : "text-[#8AA6AA]"
+                    }`}
+                  >
+                    {step.charAt(0).toUpperCase() + step.slice(1)}
+                  </p>
+                  {index < selectedContent.sequence.length - 1 && (
+                    <p className="text-[#8AA6AA] font-bold">━</p>
+                  )}
+                </React.Fragment>
+              ))}
+            </div>
+            <div>
+              <div className="font-bold ">{selectedContent?.sub}</div>
+              <div className="]">{selectedContent?.sub_content}</div>
+              <div className="hover:bg-[#8AA6AA] mt-[20px] rounded-lg md:mt-[7%] md:mb-[5%] gap-4 text-white justify-center place-items-center md:rounded-md bg-[#8AA6AA] border-[#8AA6AA] flex md:flex-row md:h-[15%] md:w-[35%] h-[50px] w-[50%]">
+                <button className="hover:bg-[#8AA6AA]">Know More</button>
+                <img src={arrow} alt="arrow" className="md:h-[20px]"></img>
               </div>
-              <p className="font-bold text-[#8AA6AA]">Blow</p>
-              <p className="text-[#8AA6AA] font-bold">━━━</p>
-              <div className="h-10 w-10 justify-center flex place-items-center md:p-2 rounded-full bg-white border border-[#8AA6AA]">
-                <img className="" src={fill}></img>
-              </div>
-              <p className=" text-[#8AA6AA]">Fill</p>
-              <p className="text-[#8AA6AA] ">━━━━━━</p>
-              <div className="h-10 w-10 justify-center flex place-items-center md:p-2 rounded-full bg-white border border-[#8AA6AA]">
-                <img className="" src={seal}></img>
-              </div>
-              <p className=" text-[#8AA6AA]">Seal</p>
             </div>
 
-
-            <div className="font-bold">{selectedContent?.sub}</div>
-            <div className="">{selectedContent?.sub_content}</div>
-            <div className="hover:bg-[#8AA6AA] mt-[20px] rounded-lg md:mt-[7%] md:mb-[5%] gap-4 text-white justify-center place-items-center md:rounded-md bg-[#8AA6AA] border-[#8AA6AA] flex md:flex-row md:h-[15%] md:w-[35%] h-[50px] w-[50%]">
-              <button className="hover:bg-[#8AA6AA]">Know More</button>
-              <img src={arrow} alt="arrow" className="md:h-[20px]"></img>
-            </div>
-
-            {/* flow chart for desktop*/}
+            {/* Flow chart for desktop */}
             <div className="hidden md:flex md:flex-row md:mt-6 place-items-center md:gap-1">
-              <div className="md:p-2 rounded-full bg-[#8AA6AA]">
-                <img className="" src={img1}></img>
-              </div>
-              <p className="font-bold text-[#8AA6AA]">Blow</p>
-              <p className="text-[#8AA6AA] font-bold">━━━</p>
-              <div className="md:p-2 rounded-full bg-white border border-[#8AA6AA]">
-                <img className="" src={fill}></img>
-              </div>
-              <p className=" text-[#8AA6AA]">Fill</p>
-              <p className="text-[#8AA6AA] ">━━━━━━</p>
-              <div className="md:p-2 rounded-full md:border-[#8AA6AA] border bg-white">
-                <img className="" src={seal}></img>
-              </div>
-              <p className=" text-[#8AA6AA]">Seal</p>
+              {selectedContent?.sequence.map((step, index) => (
+                <React.Fragment key={index}>
+                  <div
+                    className={`md:p-2 rounded-full ${
+                      index === 0 ? "bg-[#8AA6AA] text-[#8AA6AA]" : "bg-white"
+                    } ${
+                      index < selectedContent.sequence.length
+                        ? "border border-[#8AA6AA]"
+                        : ""
+                    }`}
+                  >
+                    <img
+                      src={
+                        step === "blow"
+                          ? img1
+                          : step === "fill"
+                          ? fill
+                          : step === "seal"
+                          ? seal
+                          : step === "Injection"
+                          ? injection
+                          : step === "Stretch"
+                          ? stretch
+                          : step === "Blow"
+                          ? blow_small
+                          : step === "Form"
+                          ? form
+                          : step === "Molding"
+                          ? molding
+                          : img1
+                      }
+                      alt={step}
+                    ></img>
+                  </div>
+                  <p
+                    className={`font-bold text-[#8AA6AA] ${
+                      index === 0
+                        ? "font-['ClashDisplayBold'] text-[#8AA6AA]"
+                        : "text-[#8AA6AA]"
+                    }`}
+                  >
+                    {step.charAt(0).toUpperCase() + step.slice(1)}
+                  </p>
+                  {index < selectedContent.sequence.length - 1 && (
+                    <p className="text-[#8AA6AA] font-bold">━━━</p>
+                  )}
+                </React.Fragment>
+              ))}
             </div>
           </div>
           <div className="hidden md:block ">
