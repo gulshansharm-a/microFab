e9b1cbfc24ceed369c6c71eb4520cdf2d634e2d5
nivivirat <imniveditha1188@gmail.com>
2023-09-14 22:23:53 +0530

    ongoing transition changes

--- src/components/Home/machine_frame/Machine_frame.js
+++ src/components/Home/machine_frame/Machine_frame.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Machine from "../../../assets/Machine_Frame_Images/Machine.svg";
 import "../../../App.css";
 import WFS_Card from "./WFS_Card";
 
 const Machine_Frame = () => {
+  const textArray = ["Providing Industry", "Solutions from the", "Start"];
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % textArray.length);
+    }, 3000); // Adjust the delay as needed (3 seconds in this example)
+
+    return () => clearInterval(interval); // Clean up the interval on unmount
+  }, []);
+
+
   return (
     <div className="font-['ClashDisplay'] w-full h-[700px] md:w-[97%] md:h-full md:mt-[75px] mt-[50px] md:rounded-[20px] bg-gradient-to-r from-[#e1e1e1] to-[#8ca7ab] md:m-4 mb-0 overflow-clip">
       <div className="relative w-full h-full z-0">
@@ -27,13 +39,24 @@ const Machine_Frame = () => {
         />
         <div className="hidden md:block absolute md:top-[54px] md:left-[33px] w-[260px] h-[260px] rounded-full bg-gradient-to-tr from-[#dcdede] to-[#cbd2d3]"></div>
 
-        <div class="md:hidden block relative overflow-hidden h-[200px]">
-          <p class="flex flex-col font-extrabold w-[400px] h-[200px] absolute left-[12px] top-[52px] text-white text-[35px] md:leading-[79px] tracking-tight text-left">
-            <span class="highlight-text animate-fall">Providing Industry</span>
-            <span class="highlight-text animate-fall">Solutions from the</span>
-            <span class="highlight-text animate-fall">Start</span>
-          </p>
-        </div>
+        <div className="md:hidden block relative overflow-hidden h-[200px]">
+      <p className="flex flex-col font-extrabold w-[400px] h-[200px] absolute left-[12px] top-[52px] text-white text-[35px] md:leading-[79px] tracking-tight text-left">
+        {textArray.map((text, index) => (
+          <span
+            key={index}
+            className={`highlight-text ${
+              currentWordIndex === index ? 'animate-fall' : ''
+            }`}
+            style={{
+              visibility: index === 2 || currentWordIndex === index ? 'visible' : 'hidden',
+              marginBottom: index === 2 ? 0 : '0px', // Adjust the margin as needed
+            }}
+          >
+            {text}
+          </span>
+        ))}
+      </p>
+    </div>
 
         <p className="hidden md:block w-[554px] h-[316px] absolute left-[52px] top-[52px] text-white font-bold md:text-[64px] md:leading-[79px] tracking-tight text-left">
           <span className="highlight-text ">
