6edf581590075145024eb330efc1f0660c9dd4d3
Rhitam Nayek <rhitamnayek17@gmail.com>
2023-09-28 23:59:40 +0530

    t

---
+++ src/layouts/authentication/sign-up/index.js
@@ -0,0 +1,77 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Checkbox from "@mui/material/Checkbox";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDInput from "components/MDInput";
+import MDButton from "components/MDButton";
+
+// Authentication layout components
+import CoverLayout from "layouts/authentication/components/CoverLayout";
+
+// Images
+import bgImage from "assets/images/bg-sign-up-cover.jpeg";
+
+function Cover() {
+  return (
+    <CoverLayout image={bgImage}>
+      <Card>
+        <MDBox
+          variant="gradient"
+          bgColor="info"
+          borderRadius="lg"
+          coloredShadow="success"
+          mx={2}
+          mt={-3}
+          p={3}
+          mb={1}
+          textAlign="center"
+        >
+          <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
+            Enter Articles
+          </MDTypography>
+          {/* <MDTypography display="block" variant="button" color="white" my={1}>
+            Enter your email and password to register
+          </MDTypography> */}
+        </MDBox>
+        {/* <MDBox component="form" role="form"> */}
+        <MDBox pt={4} pb={3} px={3}>
+          <MDBox component="form" role="form">
+            <MDBox mb={2}>
+              <MDInput type="text" label="Enter Title" fullWidth />
+              <MDBox mb={2}>
+                <MDInput type="text" label="Enter Description" fullWidth />
+              </MDBox>
+              <MDBox>
+                <MDInput mb={2} type="text" label="Image-link / choose from below" fullWidth />
+                <MDInput type="File" />
+              </MDBox>
+            </MDBox>
+          </MDBox>
+        </MDBox>
+      </Card>
+    </CoverLayout>
+  );
+}
+
+export default Cover;
---
+++ src/layouts/dashboard/components/OrdersOverview/index.js
@@ -0,0 +1,78 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Icon from "@mui/material/Icon";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+// Material Dashboard 2 React example components
+import TimelineItem from "examples/Timeline/TimelineItem";
+
+function OrdersOverview() {
+  return (
+    <Card sx={{ height: "100%" }}>
+      <MDBox pt={3} px={3}>
+        <MDTypography variant="h6" fontWeight="medium">
+          News overview
+        </MDTypography>
+        <MDBox mt={0} mb={2}>
+          <MDTypography variant="button" color="text" fontWeight="regular">
+            <MDTypography display="inline" variant="body2" verticalAlign="middle">
+              <Icon sx={{ color: ({ palette: { success } }) => success.main }}>arrow_upward</Icon>
+            </MDTypography>
+            &nbsp;
+            <MDTypography variant="button" color="text" fontWeight="medium">
+              24%
+            </MDTypography>{" "}
+            this month
+          </MDTypography>
+        </MDBox>
+      </MDBox>
+      <MDBox p={2}>
+        <TimelineItem
+          color="success"
+          icon="notifications"
+          title="Political news"
+          dateTime="22 DEC 7:20 PM"
+        />
+        <TimelineItem
+          color="error"
+          icon="inventory_2"
+          title="Environmental news"
+          dateTime="21 DEC 11 PM"
+        />
+        <TimelineItem
+          color="info"
+          icon="shopping_cart"
+          title="Sports news"
+          dateTime="21 DEC 9:34 PM"
+        />
+        <TimelineItem
+          color="primary"
+          icon="vpn_key"
+          title="Celeb News"
+          dateTime="18 DEC 4:54 AM"
+          lastItem
+        />
+      </MDBox>
+    </Card>
+  );
+}
+
+export default OrdersOverview;
