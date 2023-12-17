1a6e35cf370ad1ac0e604f66affab8c53ddd1e49
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2023-12-17 18:59:24 +0530

    Update Services.jsx

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -21,7 +21,7 @@ export default function Service() {
           </h3>
           <br></br><br></br><br></br>
 
-          <h3 className="mb-4  pb-4 text-4xl leading-tight text-black">
+          <h3 className="mb-4  pb-4 text-4xl leading-tight text-black zx">
           Experience the difference with us as firmly stand with our motto to offer incredible services every step of the way
           </h3>
 
@@ -29,18 +29,18 @@ export default function Service() {
           While we dabble in providing exceptional customer service, you can connect with us for project management and contract manufacturing.
           </h6>
           <br></br><br></br><br></br><br></br>
-          <div className="serv4"></div>
+          <div class="serv4"></div>
           <br></br>
           
 
           <div className="flex flex-col rounded-lg text-black md:max-w-5xl md:flex-row">
   <img
-    className="h-64 w-full rounded-t-lg object-cover  md:h-auto md:w-64 md:rounded-none md:rounded-l-lg order-2 mn md:order-"
+    className="h-64 w-full rounded-t-lg object-cover ds md:h-auto md:w-64 md:rounded-none md:rounded-l-lg order-2 mn md:order-"
     src={pro}
     alt=""
   />
   <div className="flex flex-col justify-start order-1 md:order-1">
-    <h3 className="mb-2 pb-4 text-6xl leading-tight text-black">
+    <h3 className="mb-2 pb-4 text-5xl leading-tight text-black">
     Project Construction & Management </h3>
     <h6 className="text-2xs leading-tight text-black zx " color="clack">
           While you communicate your project requirements with us, our agile and proactive team always directs and supervises project implementation right from the nascent and initial stage. Ultimately, our strength lies in customer satisfaction and meeting the business needs for a viable project with regards to the budget and the functionality.
@@ -63,7 +63,7 @@ export default function Service() {
       <br></br><br></br>
       <div className="serv1">
         <div>
-          <div className="serv4"></div>
+          <div class="serv4"></div>
           <br></br>
           
 
@@ -74,9 +74,9 @@ export default function Service() {
     alt=""
   />
   <div className="flex flex-col justify-start order-1 md:order-1">
-    <h3 className="mb-2 pb-4 text-6xl leading-tight text-black">
+    <h3 className="mb-2 pb-4 text-5xl leading-tight text-black">
     Contract manufacturing</h3>
-    <h6 className="text-2xs leading-tight text-black" color="clack">
+    <h6 className="text-2xs leading-tight text-black zx" color="clack">
     When the entire gamut of production operational activities becomes cumbersome, you can approach us to deal with everything from scratch. With MicroFab, your company is sure to save money, produce more products, and gain access to more resources. As we cater to different businesses, our mavericks are adept at maintaining machinery and the tools. We also shoulder every responsibility while designing products. </h6>
     
   </div>
@@ -86,7 +86,7 @@ export default function Service() {
 
           <div className="flex flex-col rounded-lg text-black md:max-w-5xl md:flex-row">
   <img
-    className="h-96 w-full rounded-t-lg object-cover  md:h-auto md:w-96 md:rounded-none md:rounded-l-lg order-2 bbv md:order-"
+    className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-96 md:rounded-none md:rounded-l-lg order-2 bbv md:order-"
     src={pro4}
     alt=""
   />
@@ -97,18 +97,18 @@ export default function Service() {
   />
 </div>
 <br></br><br></br><br></br><br></br>
-          <div className="serv4"></div>
+          <div class="serv4"></div>
           <br></br>
           
 
           <div className="flex flex-col rounded-lg text-black md:max-w-5xl md:flex-row">
   <img
-    className="h-64 w-full rounded-t-lg object-cover  md:h-auto md:w-64 md:rounded-none md:rounded-l-lg md:order-1"
+    className="h-64 w-full rounded-t-lg object-cover dc md:h-auto md:w-64 md:rounded-none md:rounded-l-lg md:order-1"
     src={pro}
     alt=""
   />
   <div className="flex flex-col justify-start order-1 md:order-2 xc">
-    <h3 className="mb-2 pb-4 text-6xl leading-tight text-black">
+    <h3 className="mb-2 pb-4 text-5xl leading-tight text-black">
     Advisory Services </h3>
     <h6 className="text-2xs leading-tight text-black" color="clack">
     When perplexity is still reigning when you gear up to be future ready, we advise on the technology that you need to incorporate in your organization. Eventually, the machinery must lower down the operational costs and be capable of producing an astounding number of finished products. After all, we provide packaging solutions for you to attain recognition in the competitive world. </h6>
