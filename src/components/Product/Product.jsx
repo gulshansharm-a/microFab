584360a9263ef922a90b13a50c91827bef8c92ad
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2024-01-01 09:54:29 +0530

    Update Product.jsx

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import './prod.css';
-import pro from "./images/Frame 21516.svg";
-import pro2 from "./images/Frame 21518.svg";
-import pro3 from "./images/Frame 21519.svg";
-import pro4 from "./images/Frame 21520.svg";
-import s1 from "./images/Rectangle 47.svg";
-import s2 from "./images/Rectangle 48.svg";
-import s3 from "./images/Rectangle 49.svg";
-import a1 from "./images/Frame 21515.svg";
+// import pro from "./images/Frame 21516.svg";
+import pro2 from "./images/image 47.svg";
+import pro3 from "./images/image 48.svg";
+import pro4 from "./images/image 49.svg";
+import s3 from "./images/Rectangle 49 (5).svg";
+import s1 from "./images/Rectangle 48.svg";
+import s2 from "./images/Rectangle 48 (2).svg";
+import a1 from "./images/Group 64 1.svg";
 import a2 from "./images/Frame 21516 (1).svg";
 import b1 from "./images/Rectangle 49 (1).svg";
-import b2 from "./images/Rectangle 47 (1).svg";
-import b3 from "./images/Rectangle 48 (1).svg";
+import b2 from "./images/4.svg";
+import b3 from "./images/1.svg";
+import b4 from "./images/3.svg";
 import MedicalDevicesCard from "../MedicalDevices/MedicalDevicesCard";
 import Footer from "../Common/Footer/Footer";
 import TurnKeySolutionsData from "./prodt.json";
@@ -28,23 +29,24 @@ export default function Product() {
         <br></br><br></br><br></br>
 
         <h3 className="mb-4  pb-4 text-4xl sm:text-2xl hgf leading-tight text-black  animated-box animate__animated animate__fadeInLeft">
-          Our service can be a peace of mind when you’re very much concerned about machine maintenance.</h3>
+       <strong> Our solutions ensure ease of maintaining machines</strong></h3>
 
         <h6 className="text-2xs leading-tight text-black serv2 animated-box animate__animated animate__fadeInRight" color="clack">
-          While we dabble in providing exceptional customer service, you can connect with us for project management and contract manufacturing.
+        MicroFab’s dedicated team is always present round-the-clock to assist you in every way. Our dedicated team also conducts specialized training sessions, empowering your personnel with the skills for optimal utilization of machines.
         </h6>
         <br></br><br>
         </br>
       </div>
-
-      <h3 className="mb-4 ml-12  pb-4 text-4xl leading-tight text-black vbb  animated-box animate__animated animate__fadeInLeft animate__delay-1s">
+     <div className="oo">
+      <h3 className="mb-4 pb-4 hhk text-4xl ml-4 leading-tight text-black vbb  animated-box animate__animated animate__fadeInLeft animate__delay-1s">
         Machines</h3>
-      <h6 className="text-xl leading-tight text-black vbb  animated-box animate__animated animate__fadeInRight animate__delay-1s" color="clack">
+      <h6 className="text-xl  leading-tight text-black ml-4 vbb  animated-box animate__animated animate__fadeInRight animate__delay-1s" color="clack">
         While we dabble in providing exceptional customer service, you can connect with us for project management and contract manufacturing.
       </h6>
+      </div>
       <br></br>
 
-      <div className="md:pl-16 flex flex-wrap gap-7 md:gap-4 md:gap-y-8 md:p-0 p-8 md:place-items-start place-items-center md:justify-start justify-center  animated-box animate__animated animate__fadeIn animate__delay-1s">
+      {/* <div className="md:pl-16 flex flex-wrap gap-7 md:gap-4 md:gap-y-8 md:p-0 p-8 md:place-items-start place-items-center md:justify-start justify-center  animated-box animate__animated animate__fadeIn animate__delay-1s">
         {TurnKeySolutionsData.map((device, index) => (
           <MedicalDevicesCard
             key={index} // Ensure each card has a unique key
@@ -53,7 +55,7 @@ export default function Product() {
             img={device.img}
           />
         ))}
-      </div>
+      </div> */}
       <br></br>
 
 
@@ -61,7 +63,7 @@ export default function Product() {
       <div className="serv1 animated-box animate__animated animate__fadeInRight animate__delay-1s">
         <div class="serv4"></div>
         <br></br>
-
+        <br></br><br></br>
 
         <div className="flex flex-col rounded-lg text-black md:max-w-5xl md:flex-row nnnn">
           <img
@@ -71,21 +73,22 @@ export default function Product() {
           />
 
           <img
-            className="w-full df zz rounded-t-lg dffp object-cover  md:h-auto md:w-96 md:rounded-none md:rounded-l-lg md:order-1"
+            className="w-full df zz rounded-t-lg dffp  object-cover -ml-8  md:h-auto md:w-96 md:rounded-none md:rounded-l-lg md:order-1"
             src={s2}
             alt=""
           />
           <img
-            className="w-full rounded-t-lg object-cover  md:h-auto md:w-64 md:rounded-none md:rounded-l-lg md:order-1 as"
+            className="w-full rounded-t-lg object-cover md:h-auto md:w-64 md:rounded-none md:rounded-l-lg md:order-1 as"
             src={s1}
             alt=""
           />
-          <div className="flex flex-col justify-start order-1 md:order-1 xd gh">
-            <h3 className="mb-2 pb-4 text-3xl leading-tight text-black xd bh">
+          <div className="flex flex-col justify-start order-1 md:order-1 xd gh ">
+            <h3 className="mb-2 pb-4 text-5xl leading-tight text-black xd bh zs -mr-3">
+              
               After sale
               service </h3>
-            <h6 className="text-2xs leading-tight text-black serv7" color="clack">
-              In today’s era, it’s essential to maintain even the best technology. So, with our comprehensive services, you can stay confident that the system offers better quality and runs smoothly and economically. While we assure you that the machine functions smoothly, our team also helps the workforce to know more about the processes that drive the production flow.
+            <h6 className="text-xl leading-tight text-black serv7 -mr-64 bh fui" color="clack">
+            At MicroFab, our dedication to excellence extends beyond the point of purchase. We take pride in providing comprehensive after-sales services to ensure that your experience with our products and services remain seamless, reliable, and maximally productive.
             </h6></div>
         </div>
       </div>
@@ -109,7 +112,7 @@ export default function Product() {
         />
 
         <div className="flex flex-col justify-start order-2 md:order-2 xd gh  animated-box animate__animated animate__fadeInLeft animate__delay-3s">
-          <h3 className="mb-2 pb-4 text-3xl leading-tight text-black xd fgi">
+          <h3 className="mb-2 pb-4 text-5xl md:text-5xl sm:text-3xl leading-tight text-black xd fgi">
             Machine
             upgradisation</h3>
           <h6 className="text-xl leading-tight text-black xd fgi" color="clack">
@@ -119,35 +122,37 @@ export default function Product() {
         <div>
           <br></br><br></br><br></br><br></br><br></br>
           <div class="serv4"></div>
-          <br></br>
+          <br></br><br></br><br></br>
 
 
           <div className="flex flex-col rounded-lg text-black md:max-w-5xl md:flex-row">
             <img
-              className="h-120 w-full animated-box animate__animated animate__fadeInRight animate__delay-4s rounded-t-lg object-cover  md:h-auto lg:w-120 md:w-96 md:rounded-none md:rounded-l-lg order-2 mmn md:order-"
+              className="h-120 w-full gghh cvf animated-box animate__animated animate__fadeInRight animate__delay-4s rounded-t-lg object-cover  md:h-auto lg:w-120 md:w-96 md:rounded-none md:rounded-l-lg order-2 mmn md:order-"
               src={pro2}
               alt=""
             />
 
             <div className="flex flex-col justify-start order-1 md:order-1 gghh  animated-box animate__animated animate__fadeInRight animate__delay-4s">
-              <h3 className="mb-2 pb-4 md:text-3xl text-3xl hhjj  leading-tight text-black">
-                Contract manufacturing</h3>
-              <h6 className="text-2xs sdd leading-tight text-black " color="clack">
-                When the entire gamut of production operational activities becomes cumbersome, you can approach us to deal with everything from scratch. With MicroFab, your company is sure to save money, produce more products, and gain access to more resources. As we cater to different businesses, our mavericks are adept at maintaining machinery and the tools. We also shoulder every responsibility while designing products. </h6>
+              <h3 className="mb-2 pb-4 md:text-5xl text-5xl hhjj  leading-tight text-black">
+              Refurbished Machines</h3>
+              <h6 className="text-xl sdd leading-tight text-black fui" color="clack">
+              Our refurbished machines offer the perfect blend of performance and value. By breathing new life into pre-owned equipment, we make cutting-edge technology accessible at a fraction of the cost, all while promoting eco-friendly practices. 
+  </h6><br></br><h6>
+Note: The images used for this section is for reference purpose only</h6>
 
             </div>
           </div>
           <br></br>
 
 
-          <div className="flex flex-col rounded-lg text-black md:max-w-5xl md:flex-row  animated-box animate__animated animate__fadeInLeft animate__delay-5s">
+          <div className="flex flex-col  rounded-lg gghh text-black md:max-w-5xl md:flex-row  animated-box animate__animated animate__fadeInLeft animate__delay-5s">
             <img
-              className="h-96 w-full rounded-t-lg object-cover  md:h-auto md:w-96 md:rounded-none md:rounded-l-lg order-2 bbv animated-box animate__animated animate__fadeInLeft animate__delay-5s md:order-"
+              className="h-96 w-full  rounded-t-lg  object-cover  md:h-auto md:w-96 md:rounded-none md:rounded-l-lg order-2 bbv animated-box animate__animated animate__fadeInLeft animate__delay-5s md:order-"
               src={pro4}
               alt=""
             />
             <img
-              className="h-64 w-full rounded-t-lg object-cover  md:h-auto md:w-120 md:rounded-none md:rounded-l-lg order-2 bv animated-box animate__animated animate__fadeInLeft animate__delay-5s md:order-1"
+              className="h-64 w-full cvf rounded-t-lg  object-cover  md:h-auto md:w-120 md:rounded-none md:rounded-l-lg order-2 bv animated-box animate__animated animate__fadeInLeft animate__delay-5s md:order-1"
               src={pro3}
               alt=""
             />
@@ -167,25 +172,33 @@ export default function Product() {
     alt=""
   /> */}
             <img
-              className="w-full ml-96 dff rounded-t-lg object-cover  md:h-auto md:w-64 md:rounded-none md:rounded-l-lg md:order-3"
+              className="w-full ml-96 gghh fdr rounded-t-lg object-cover  md:h-auto md:w-64 md:rounded-none md:rounded-l-lg md:order-3"
               src={b2}
               alt=""
             />
             <img
-              className="w-full m-12 ase rounded-t-lg object-cover md:h-auto md:w-64 md:rounded-none md:rounded-l-lg md:order-3"
+              className="w-56 m-8 h-64 ml-24 fdr -mt-2 rounded-t-lg object-cover  md:w-56 md:rounded-none md:rounded-l-lg md:order-3"
               src={b3}
               alt=""
             />
-            <div className="flex flex-col justify-start order-1 md:order-1 xv xdd ">
-              <h3 className="mb-2 pb-4 text-3xl leading-tight text-black hdfc fgi">
+             <img
+              className="w-96 m-8 h-48 fdr mt-64 -ml-64 rounded-t-lg object-cover  md:w-96 md:rounded-none md:rounded-l-lg md:order-3"
+              src={b4}
+              alt=""
+            />
+            <div className="flex flex-col justify-start order-1 md:order-1 xd xdd ">
+              <h3 className="mb-2 pb-4 text-5xl leading-tight text-black hdfc fgi xd">
                 Medical devices</h3>
-              <h6 className="text-xl leading-tight text-black hdfc fgi" color="clack">
+              <h6 className="text-xl leading-tight text-black hdfc fgi -mr-64" color="clack">
 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt . </h6>
+              In the healthcare sector, MicroFab understands the criticality of
+choosing quality medical equipment. While serving patients, we
+ensure that the device is safe to use. We also develop designs for
+devices which are apt for using, with comfort. </h6>
               <a
               href="/medicalDevices"
                 type="submit"
-                className="bg-[#8AA6AA] mt-4 md:mt-4 text-white px-4 py-2  w-[200px] rounded-md"
+                className="bg-[#8AA6AA] mt-4 md:mt-4 text-white px-4 py-2 -ml-6  w-[200px] rounded-md"
               >
               KNOW MORE ->
               </a>
